21e5128499ba503cdcd2fae7a6761c2677ff31bf
CD-Z <69157453+CD-Z@users.noreply.github.com>
2025-08-11 22:58:55 +0200

    added Queue Context

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ enableFreeze(true);
 
 import React, { useRef } from 'react';
 import { StatusBar, StyleSheet } from 'react-native';
+import { NavigationContainerRef } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import LottieSplashScreen from 'react-native-lottie-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -13,20 +14,11 @@ import * as Notifications from 'expo-notifications';
 
 import { createTables } from '@database/db';
 import AppErrorBoundary from '@components/AppErrorBoundary/AppErrorBoundary';
-import { ThemeContextProvider } from '@providers/ThemeProvider';
 import { RootStackParamList } from '@navigators/types';
 
 import Main from './src/navigators/Main';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-
-// Rozenite DevTools
-import { useReactNavigationDevTools } from '@rozenite/react-navigation-plugin';
-import { useNetworkActivityDevTools } from '@rozenite/network-activity-plugin';
-import { usePerformanceMonitorDevTools } from '@rozenite/performance-monitor-plugin';
-import { useMMKVDevTools } from '@rozenite/mmkv-plugin';
-import { store } from '@plugins/helpers/storage';
-import { MMKVStorage } from '@utils/mmkv/mmkv';
-import { NavigationContainerRef } from '@react-navigation/native';
+import { Providers } from './src/providers/Providers';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -44,17 +36,9 @@ LottieSplashScreen.hide();
 const App = () => {
   const navigationRef = useRef<NavigationContainerRef<RootStackParamList>>(null);
 
-  // Enable React Navigation DevTools in development
-  useReactNavigationDevTools({ ref: navigationRef });
-  useNetworkActivityDevTools();
-  usePerformanceMonitorDevTools();
-  useMMKVDevTools({
-    storages: [store, MMKVStorage],
-  });
-
   return (
     <GestureHandlerRootView style={styles.flex}>
-      <ThemeContextProvider>
+      <Providers>
         <AppErrorBoundary>
           <SafeAreaProvider>
             <PaperProvider>
@@ -65,7 +49,7 @@ const App = () => {
             </PaperProvider>
           </SafeAreaProvider>
         </AppErrorBoundary>
-      </ThemeContextProvider>
+      </Providers>
     </GestureHandlerRootView>
   );
 };
