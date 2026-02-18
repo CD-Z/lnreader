41b1cecbbb1e31f1e8ea78e04cc1e9f1191e6eaa
CD-Z <69157453+CD-Z@users.noreply.github.com>
2026-02-18 11:53:06 +0100

    fix lint errors

--- src/utils/showToast.ts
+++ src/utils/showToast.ts
@@ -3,6 +3,7 @@ import { ToastAndroid } from 'react-native';
 export const showToast = (message: string) => {
   ToastAndroid.show(message, ToastAndroid.SHORT);
   if (__DEV__) {
+    // eslint-disable-next-line no-console
     console.trace('Toast: ', message);
   }
 };
