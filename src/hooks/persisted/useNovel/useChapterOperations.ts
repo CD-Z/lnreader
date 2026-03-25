5c3d97bfbb3d3d4e0b5a9608b4ca711caeb2f4fa
CD-Z <github.verify097@passmail.com>
2026-03-25 18:15:29 +0100

    refactor: move chapter mutations into store-ready action helpers

--- src/hooks/persisted/useNovel/useChapterOperations.ts
+++ src/hooks/persisted/useNovel/useChapterOperations.ts
@@ -1,20 +1,18 @@
 import { useCallback } from 'react';
 import { ChapterFilterKey, ChapterOrderKey } from '@database/constants';
 import {
-  bookmarkChapter as _bookmarkChapter,
-  deleteChapter as _deleteChapter,
-  deleteChapters as _deleteChapters,
-  getPageChapters as _getPageChapters,
-  markChapterRead as _markChapterRead,
-  markChaptersRead as _markChaptersRead,
-  markChaptersUnread as _markChaptersUnread,
-  markPreviousChaptersUnread as _markPreviousChaptersUnread,
-  markPreviuschaptersRead as _markPreviuschaptersRead,
-  updateChapterProgress as _updateChapterProgress,
-} from '@database/queries/ChapterQueries';
+  bookmarkChaptersAction,
+  deleteChapterAction,
+  deleteChaptersAction,
+  markChapterReadAction,
+  markChaptersReadAction,
+  markChaptersUnreadAction,
+  markPreviouschaptersReadAction,
+  markPreviousChaptersUnreadAction,
+  refreshChaptersAction,
+  updateChapterProgressAction,
+} from './chapterActions';
 import { ChapterInfo, NovelInfo } from '@database/types';
-import { getString } from '@strings/translations';
-import { showToast } from '@utils/showToast';
 
 export interface UseChapterOperationsParams {
   novel: NovelInfo | undefined;
@@ -74,196 +72,85 @@ export const useChapterOperations = ({
 
   const bookmarkChapters = useCallback(
     (_chapters: ChapterInfo[]) => {
-      _chapters.forEach(_chapter => {
-        _bookmarkChapter(_chapter.id);
-      });
-      mutateChapters(chs =>
-        chs.map(chapter => {
-          if (_chapters.some(_c => _c.id === chapter.id)) {
-            return {
-              ...chapter,
-              bookmark: !chapter.bookmark,
-            };
-          }
-          return chapter;
-        }),
-      );
+      bookmarkChaptersAction(_chapters, mutateChapters);
     },
     [mutateChapters],
   );
 
   const markPreviouschaptersRead = useCallback(
     (chapterId: number) => {
-      if (novel) {
-        _markPreviuschaptersRead(chapterId, novel.id);
-        mutateChapters(chs =>
-          chs.map(chapter =>
-            chapter.id <= chapterId ? { ...chapter, unread: false } : chapter,
-          ),
-        );
-      }
+      markPreviouschaptersReadAction(chapterId, novel, mutateChapters);
     },
     [mutateChapters, novel],
   );
 
   const markChapterRead = useCallback(
     (chapterId: number) => {
-      _markChapterRead(chapterId);
-
-      mutateChapters(chs =>
-        chs.map(c => {
-          if (c.id !== chapterId) {
-            return c;
-          }
-          return {
-            ...c,
-            unread: false,
-          };
-        }),
-      );
+      markChapterReadAction(chapterId, mutateChapters);
     },
     [mutateChapters],
   );
 
   const updateChapterProgress = useCallback(
     (chapterId: number, progress: number) => {
-      _updateChapterProgress(chapterId, Math.min(progress, 100));
-
-      mutateChapters(chs =>
-        chs.map(c => {
-          if (c.id !== chapterId) {
-            return c;
-          }
-          return {
-            ...c,
-            progress,
-          };
-        }),
-      );
+      updateChapterProgressAction(chapterId, progress, mutateChapters);
     },
     [mutateChapters],
   );
 
   const markChaptersRead = useCallback(
     (_chapters: ChapterInfo[]) => {
-      const chapterIds = _chapters.map(chapter => chapter.id);
-      _markChaptersRead(chapterIds);
-
-      mutateChapters(chs =>
-        chs.map(chapter => {
-          if (chapterIds.includes(chapter.id)) {
-            return {
-              ...chapter,
-              unread: false,
-            };
-          }
-          return chapter;
-        }),
-      );
+      markChaptersReadAction(_chapters, mutateChapters);
     },
     [mutateChapters],
   );
 
   const markPreviousChaptersUnread = useCallback(
     (chapterId: number) => {
-      if (novel) {
-        _markPreviousChaptersUnread(chapterId, novel.id);
-        mutateChapters(chs =>
-          chs.map(chapter =>
-            chapter.id <= chapterId ? { ...chapter, unread: true } : chapter,
-          ),
-        );
-      }
+      markPreviousChaptersUnreadAction(chapterId, novel, mutateChapters);
     },
     [mutateChapters, novel],
   );
 
   const markChaptersUnread = useCallback(
     (_chapters: ChapterInfo[]) => {
-      const chapterIds = _chapters.map(chapter => chapter.id);
-      _markChaptersUnread(chapterIds);
-
-      mutateChapters(chs =>
-        chs.map(chapter => {
-          if (chapterIds.includes(chapter.id)) {
-            return {
-              ...chapter,
-              unread: true,
-            };
-          }
-          return chapter;
-        }),
-      );
+      markChaptersUnreadAction(_chapters, mutateChapters);
     },
     [mutateChapters],
   );
 
   const deleteChapter = useCallback(
     (_chapter: ChapterInfo) => {
-      if (novel) {
-        _deleteChapter(novel.pluginId, novel.id, _chapter.id).then(() => {
-          mutateChapters(chs =>
-            chs.map(chapter => {
-              if (chapter.id !== _chapter.id) {
-                return chapter;
-              }
-              return {
-                ...chapter,
-                isDownloaded: false,
-              };
-            }),
-          );
-          showToast(getString('common.deleted', { name: _chapter.name }));
-        });
-      }
+      deleteChapterAction(_chapter, novel, mutateChapters);
     },
     [mutateChapters, novel],
   );
 
   const deleteChapters = useCallback(
     (_chapters: ChapterInfo[]) => {
-      if (novel) {
-        _deleteChapters(novel.pluginId, novel.id, _chapters).then(() => {
-          showToast(
-            getString('updatesScreen.deletedChapters', {
-              num: _chapters.length,
-            }),
-          );
-          mutateChapters(chs =>
-            chs.map(chapter => {
-              if (_chapters.some(_c => _c.id === chapter.id)) {
-                return {
-                  ...chapter,
-                  isDownloaded: false,
-                };
-              }
-              return chapter;
-            }),
-          );
-        });
-      }
+      deleteChaptersAction(_chapters, novel, mutateChapters);
     },
     [novel, mutateChapters],
   );
 
   const refreshChapters = useCallback(() => {
-    if (novel?.id && !fetching) {
-      _getPageChapters(
-        novel.id,
-        settingsSort,
-        settingsFilter,
-        currentPage,
-      ).then(chs => {
-        transformAndSetChapters(chs);
-      });
-    }
+    refreshChaptersAction({
+      novel,
+      fetching,
+      settingsSort,
+      settingsFilter,
+      currentPage,
+      transformChapters,
+      setChapters: _setChapters,
+    });
   }, [
-    novel?.id,
+    novel,
     fetching,
     settingsSort,
     settingsFilter,
     currentPage,
-    transformAndSetChapters,
+    transformChapters,
+    _setChapters,
   ]);
 
   return {
